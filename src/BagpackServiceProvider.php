a9eab61cf971a4c6db39e449cbb7490e0e1551ba
Gabriel Nwogu <gabriel@ulesson.com>
2020-11-12 20:38:10 +0100

    extend migrator

--- src/BagpackServiceProvider.php
+++ src/BagpackServiceProvider.php
@@ -18,7 +18,7 @@ class BagpackServiceProvider extends ServiceProvider
         // The migrator is responsible for actually running and rollback the migration
         // files in the application. We'll pass in our database connection resolver
         // so the migrator can resolve any of these connections when it needs to.
-        $this->app->singleton('migrator', function ($app) {
+        $this->app->extend('migrator', function ($migrator, $app) {
             $repository = $app['migration.repository'];
 
             return new Migrator($repository, $app['db'], $app['files'], $app['events']);
